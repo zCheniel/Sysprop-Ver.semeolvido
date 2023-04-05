93567090210b1bd8da0fe3a2175bfe652e88e208
Daniel Sandrea <dsandrea13@gmail.com>
2023-04-05 02:46:28 -0400

    firts commit

---
+++ src/pages/compras.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function compras() {
+  return (
+    <div>compras</div>
+  )
+}
+
+export default compras
---
+++ src/pages/mantenimiento.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function mantenimiento() {
+  return (
+    <div>mantenimiento</div>
+  )
+}
+
+export default mantenimiento
